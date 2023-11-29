6c5e64db3dfdfc44ec2de809835b2f245682e4af
Jarek Karwowski <chromedivizer@gmail.com>
2023-11-29 21:20:37 +0100

    `scenarios` - reworked the `012_overtaking` scenario, adjusted actors initial config and scenario params in the launch file

--- tiago_social_scenarios_sim/src/lab_012_overtaking_hubero.cpp
+++ tiago_social_scenarios_sim/src/lab_012_overtaking_hubero.cpp
@@ -28,76 +28,104 @@ int main(int argc, char** argv) {
 	if ( argc >= 2 ) {
 		launch_delay = std::stoi(argv[1]);
 	}
-	double robot_actor_distance_threshold = ROBOT_ACTOR_DISTANCE_THRESHOLD_DEFAULT;
+	double robot_actor1_distance_threshold = ROBOT_ACTOR_DISTANCE_THRESHOLD_DEFAULT;
 	if (argc >= 3) {
-		robot_actor_distance_threshold = std::stod(argv[2]);
+		robot_actor1_distance_threshold = std::stod(argv[2]);
+	}
+	double robot_actor2_distance_threshold = ROBOT_ACTOR_DISTANCE_THRESHOLD_DEFAULT;
+	if (argc >= 4) {
+		robot_actor2_distance_threshold = std::stod(argv[3]);
 	}
 	ROS_INFO(
-		"Actor's movement will start once the robot will violate the %f m distance threshold between them",
-		robot_actor_distance_threshold
+		"Actor1 movement will start once the robot will violate the %f m distance threshold between them",
+		robot_actor1_distance_threshold
+	);
+	ROS_INFO(
+		"Actor2 movement will start once the robot will violate the %f m distance threshold between them",
+		robot_actor2_distance_threshold
 	);
 
 	// create interfaces to request tasks from actors
 	hubero::TaskRequestRosApi actor1("actor1");
 	hubero::TaskRequestRosApi actor2("actor2");
 
-	// variable that will trigger the movement of actor2
+	// wait
+	ROS_INFO("TaskRequestRos APIs fired up, scenario execution will start in %lu seconds", launch_delay);
+	hubero::TaskRequestRosApi::waitRosTime(launch_delay);
+
+	// a thread handling the movement of actor1 in the first stage of the scenario
+	std::thread a1_wp_follow_handler;
+	std::function<void()> fun_dist_tracker_robot_a1 = [&actor1, &a1_wp_follow_handler]() {
+		ROS_INFO("[SCENARIO] Firing up the execution! The Actor1 will start the first stage of the scenario");
+		a1_wp_follow_handler = actor1.moveThroughWaypoints(
+			std::vector<std::pair<Vector3, double>>{
+				{{+2.10, -1.10, 0.00}, +2.35 - IGN_PI_2} // near the fridge
+			},
+			TF_FRAME_REF
+		);
+		ROS_INFO("[SCENARIO] Waiting until Actor1 finishes his movement");
+	};
+	auto dist_tracker_robot_a1 = hubero::DistanceTracker(
+		std::cref(actor1),
+		"/mobile_base_controller/odom",
+		std::cref(robot_actor1_distance_threshold),
+		std::ref(fun_dist_tracker_robot_a1)
+	);
+
+	// atomic variable that will trigger the movement of actor2
 	std::atomic<bool> robot_close_enough_a2;
-	robot_close_enough_a2 = false;
+	robot_close_enough_a2.store(false);
 	std::function<void()> fun_dist_tracker_robot_a2 = [&robot_close_enough_a2]() {
-		robot_close_enough_a2 = true;
+		robot_close_enough_a2.store(true);
 	};
 	// subscribe robot's movement feedback to trigger the start of movement
 	auto dist_tracker_robot_a2 = hubero::DistanceTracker(
 		std::cref(actor2),
 		"/mobile_base_controller/odom",
-		std::cref(robot_actor_distance_threshold),
+		std::cref(robot_actor2_distance_threshold),
 		std::ref(fun_dist_tracker_robot_a2)
 	);
 
-	// wait
-	ROS_INFO("TaskRequestRos APIs fired up, scenario execution will start in %lu seconds", launch_delay);
-	hubero::TaskRequestRosApi::waitRosTime(launch_delay);
-
-	// moving through waypoints
-	ROS_INFO("[SCENARIO] Firing up the execution! The Actor1 will approach his goal pose");
-	std::thread a1_wp_follow_handler = actor1.moveThroughWaypoints(
-		std::vector<std::pair<Vector3, double>>{
-			{{+2.85, -1.50, 0.00}, +3.00 - IGN_PI_2} // locker midway, on the left of the passage
-		},
-		TF_FRAME_REF
-	);
-
-	ROS_INFO("[SCENARIO] Waiting before making the Actor2 move");
-	while (!robot_close_enough_a2) {
-		hubero::TaskRequestRosApi::waitRosTime(0.5);
+	ROS_INFO("[SCENARIO] Waiting until Actor1 task starts...");
+	while (!actor1.isThreadExecuting()) {
+		hubero::TaskRequestRosApi::waitRosTime(0.1);
 	}
 
+	ROS_INFO("[SCENARIO] The task of Actor1 has started");
+	// wait until the actor1 finishes movement (approaching somewhere near the actor2)
 	if (a1_wp_follow_handler.joinable()) {
 		a1_wp_follow_handler.join();
 	}
+
+	// synchronization - wait until the robot approaches actor2 close enough
+	while (!robot_close_enough_a2.load()) {
+		hubero::TaskRequestRosApi::waitRosTime(0.1);
+	}
+	ROS_INFO("[SCENARIO] Starting the 2nd stage of the scenario");
+
+	// both actors start moving so the robot has to manoeuver between them to overtake
+	ROS_INFO("[SCENARIO] Actor1 continues moving through waypoints");
 	a1_wp_follow_handler = actor1.moveThroughWaypoints(
 		std::vector<std::pair<Vector3, double>>{
-			{{+2.75, +3.20, 0.00}, +3.14 - IGN_PI_2}, // midway
-			{{-1.55, +4.80, 0.00}, -2.84 - IGN_PI_2}  // near the desk in the workshop corner
+			{{+1.85, +2.00, 0.00}, +3.14 - IGN_PI_2}, // midway
+			{{+1.60, +4.00, 0.00}, -2.50 - IGN_PI_2}  // near the workshop
 		},
 		TF_FRAME_REF
 	);
 
-	ROS_INFO("[SCENARIO] The Actor2 will approach his goal pose");
+	ROS_INFO("[SCENARIO] Actor2 starts moving through waypoints");
 	std::thread a2_wp_follow_handler = actor2.moveThroughWaypoints(
 		std::vector<std::pair<Vector3, double>>{
-			{{+1.85, +2.00, 0.00}, +3.14 - IGN_PI_2}, // midway
 			{{+2.35, +5.00, 0.00}, +3.14 - IGN_PI_2}  // near the workshop
 		},
 		TF_FRAME_REF
 	);
 
 	ROS_INFO("[SCENARIO] Waiting until both actors approach their goal poses");
-
+	dist_tracker_robot_a1.getThread().join();
+	dist_tracker_robot_a2.getThread().join();
 	a1_wp_follow_handler.join();
 	a2_wp_follow_handler.join();
-	dist_tracker_robot_a2.getThread().join();
 
 	ROS_INFO("Scenario operation finished!");
 	return 0;
